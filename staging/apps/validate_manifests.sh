6d66c24cd884ba6be6f25130965e39dc951ab9c3
Yann MARCOU <creativeyann17@gmail.com>
2022-01-13 16:43:41 -0500

    feat: CLIN-687 - add spark-jobs + cleanup-jobs

--- staging/apps/validate_manifests.sh
+++ staging/apps/validate_manifests.sh
@@ -3,6 +3,9 @@
 set -e
 
 for DIR in $(ls -d */ | cut -f1 -d'/'); do
+  if [ "$DIR" = "spark-jobs" ] || [ "$DIR" = "cleanup-jobs" ]; then
+    continue
+  fi
   echo "Verifying manifests in $DIR"
   $(cd $DIR; kustomize build . > /dev/null)
 done 
